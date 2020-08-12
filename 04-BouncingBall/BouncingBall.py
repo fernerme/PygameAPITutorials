ee68b9f0ed6d876eb72bdb503a92728913606084
fernerme <fernerme@users.noreply.github.com>
2020-08-12 14:25:27 -0400

    base objectives complete!!

--- 04-BouncingBall/BouncingBall.py
+++ 04-BouncingBall/BouncingBall.py
@@ -8,6 +8,49 @@ import random
 # TODO: Create a Ball class.
 # TODO: Possible member variables: screen, color, x, y, radius, speed_x, speed_y
 # TODO: Methods: __init__, draw, move
+class Ball:
+    def __init__(self, screen, x, y, color, radius, speed_x, speed_y):
+        """ Creates a ball that will bounce around the screen. Speed CANNOT be zero."""
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.color = color
+        self.radius = radius
+        self.speed_x = speed_x
+        self.speed_y = speed_y
+
+    def draw(self):
+        """ Draws ball onto the screen."""
+        pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
+
+    def move(self):
+        """ Controls the movement of the ball."""
+        if self.speed_x > 0:
+            if self.x + self.speed_x <= self.screen.get_width() - self.radius:
+                self.x += self.speed_x
+            else:
+                self.x = self.screen.get_width() - self.radius
+                self.speed_x = -self.speed_x
+        else:
+            if self.x + self.speed_x >= self.radius:
+                self.x += self.speed_x
+            else:
+                self.x = self.radius
+                self.speed_x = -self.speed_x
+
+        if self.speed_y > 0:
+            if self.y + self.speed_y <= self.screen.get_height() - self.radius:
+                self.y += self.speed_y
+            else:
+                self.y = self.screen.get_height() - self.radius
+                self.speed_y = -self.speed_y
+        else:
+            if self.y + self.speed_y >= self.radius:
+                self.y += self.speed_y
+            else:
+                self.y = self.radius
+                self.speed_y = -self.speed_y
+
 
 
 def main():
@@ -17,7 +60,8 @@ def main():
     screen.fill(pygame.Color('gray'))
     clock = pygame.time.Clock()
 
-    # TODO: Create an instance of the Ball class called ball1
+    # DONE: Create an instance of the Ball class called ball1
+    ball1 = Ball(screen, 20, 20, (0, 0, 0), 10, 3, 2)
 
     while True:
         for event in pygame.event.get():
@@ -28,8 +72,9 @@ def main():
         screen.fill(pygame.Color('gray'))
 
         # TODO: Move the ball
+        ball1.move()
         # TODO: Draw the ball
-
+        ball1.draw()
         pygame.display.update()
 
 
